0811da4d56271f308ef40fea50b9b6c6ad4b1c16
Augusto Pedroso <83038881+pedroso36@users.noreply.github.com>
2021-11-05 16:54:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'pedroso36',
+  youtube: 'UCUZDq458Mb0sEwUBhQnXuTw',
+  instagram: 'guto_36',
+  facebook: 'carlacoelho',
+  twitter: 'ccc'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com.br/${linksSocialMedia[social]}`
+
+    // alert(li.children[0].href)
+  }
+}
+changeSocialMediaLinks()
+
+function getGithubProfileInfo() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(date => {
+      userName.textContent = date.name
+      userBio.textContent = date.bio
+      userLink.href = date.html_url
+      profilePhoto.src = date.avatar_url
+      userLogin.textContent = date.login
+    })
+}
+
+getGithubProfileInfo()
